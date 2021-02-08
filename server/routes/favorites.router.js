f1a444a4c1ca04de02d7091fac502c694c20979f
Simeon Willard <swillard4597@gmail.com>
2021-02-08 15:23:36 -0600

    built favorites.router GET

--- server/routes/favorites.router.js
+++ server/routes/favorites.router.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
 // get favorites from db
-router.get('/', (req, res) => {
-  // GET 
-  
+router.get('/', rejectUnauthenticated, (req, res) => {
+    // GET 
+    const queryText = `SELECT "favorite_recipes" WHERE "user_id" = $1;`;
+
+    pool.query(queryText, [req.user.id])
+        .then((response) => {
+            console.log('current user: ', req.user);
+            res.send(response.rows);
+        })
+        .catch((error) => {
+            console.log(`error getting favorites ${error}`);
+            res.sendStatus(500);
+        })
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+    // POST route code here
 });
 
 module.exports = router;
