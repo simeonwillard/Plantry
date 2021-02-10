b0b4757adeb7a0a319ce230a6ee7baba2ec4b7a4
Simeon Willard <swillard4597@gmail.com>
2021-02-10 10:16:37 -0600

    user can now edit item, but need to dispatch update

--- src/components/PantryPage/PantryPage.jsx
+++ src/components/PantryPage/PantryPage.jsx
@@ -12,14 +12,28 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles({
+
+
+const useStyles = makeStyles((theme) => ({
     table: {
         minWidth: 650,
     },
-});
-
-
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 
 
@@ -30,33 +44,78 @@ function PantryPage() {
 
     const [editItem, setEditItem] = useState({
         item: '',
-        staple: false,
-        refrigerated: false,
+        staple: '',
+        refrigerated: '',
         date_purchased: ''
-    })
+    });
+
     const [edit, setEdit] = useState(false);
     const pantry = useSelector(state => state.pantryReducer);
     const dispatch = useDispatch();
-    console.log(pantry.is_staple)
+
     useEffect(() => {
         dispatch({ type: 'FETCH_PANTRY' });
     }, [])
 
 
+    const handleChange = (event) => {
+        setEditItem({ ...editItem, [event.target.name]: event.target.value });
+    }
+
+    const handleEdit = () => {
+        console.log(editItem);
+        setEdit(!edit);
+    }
+
     return (
         <div>
             <div>
-                {edit && 
-                <form type="submit">
-                    <input 
-                    type="text"
-                    placeholder="item"
-                    value={editItem}
-                    name="item"
-                    onChange={handleChange}
-                    />
-                    
-                </form>
+                {edit &&
+                    <div>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel>Item</InputLabel>
+                            <Input
+                                type="text"
+                                value={editItem.item}
+                                name="item"
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel>Staple</InputLabel>
+                            <Select
+                                value={editItem.staple}
+                                name="staple"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={true}>Yes</MenuItem>
+                                <MenuItem value={false}>No</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel>Refrigerated</InputLabel>
+                            <Select
+                                value={editItem.refrigerated}
+                                name="refrigerated"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={true}>Yes</MenuItem>
+                                <MenuItem value={false}>No</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <FormHelperText>Date Purchased</FormHelperText>
+                            <Input
+                                value={editItem.date_purchased}
+                                type="date"
+                                name="date_purchased"
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <Button variant="contained" onClick={handleEdit}>
+                            Update
+                        </Button>
+                    </div>
                 }
             </div>
             <div>
@@ -76,8 +135,8 @@ function PantryPage() {
                             {pantry.map((cupboard) => (
                                 <TableRow key={cupboard.id}>
                                     <TableCell>{cupboard.item}</TableCell>
-                                    {cupboard.staple ? <TableCell>yes</TableCell> : <TableCell>no</TableCell>}
-                                    {cupboard.refrigerated ? <TableCell>yes</TableCell> : <TableCell>no</TableCell>}
+                                    {cupboard.staple ? <TableCell>Yes</TableCell> : <TableCell>No</TableCell>}
+                                    {cupboard.refrigerated ? <TableCell>Yes</TableCell> : <TableCell>No</TableCell>}
                                     <TableCell>{cupboard.date_purchased}</TableCell>
                                     <TableCell><PantryEdit edit={edit} setEdit={setEdit} cupboard={cupboard} /></TableCell>
                                     <TableCell><PantryDelete cupboard={cupboard} /></TableCell>
